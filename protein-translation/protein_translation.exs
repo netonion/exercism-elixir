a104163ca4f1374c567d2d1a0390bb1268020856
Sam Guo <netonion@hotmail.com>
2017-07-16 20:47:21 -0400

    Exercise 6 with recursion

--- protein-translation/protein_translation.exs
+++ protein-translation/protein_translation.exs
@@ -1,23 +1,39 @@
 defmodule ProteinTranslation do
+  @mappings %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP",
+  }
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: { atom,  list(String.t()) }
+  def of_rna "" do {:ok, []} end
   def of_rna(rna) do
-    proteins = rna
-    |> String.to_charlist
-    |> Enum.chunk(3)
-    |> Enum.map(&List.to_string/1)
-    |> Enum.map(&of_codon/1)
-
-    if Enum.any?(proteins, fn {x, _} -> x == :error end) do
-      {:error, "invalid RNA"}
-    else
-      {:ok,
-        proteins
-        |> Enum.map(fn {_, x} -> x end)
-        |> Enum.take_while(&(&1 != "STOP"))
-      }
+    {codon, rest} = String.split_at(rna, 3)
+    case of_codon(codon) do
+      {:error, _} -> {:error, "invalid RNA"}
+      {:ok, "STOP"} -> {:ok, []}
+      {:ok, protein} ->
+        case of_rna(rest) do
+          {:error, _} -> {:error, "invalid RNA"}
+          {:ok, proteins} -> {:ok, [protein | proteins]}
+        end
     end
   end
 
@@ -44,26 +60,6 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: { atom, String.t() }
   def of_codon(codon) do
-    dict = %{
-      "UGU" => "Cysteine",
-      "UGC" => "Cysteine",
-      "UUA" => "Leucine",
-      "UUG" => "Leucine",
-      "AUG" => "Methionine",
-      "UUU" => "Phenylalanine",
-      "UUC" => "Phenylalanine",
-      "UCU" => "Serine",
-      "UCC" => "Serine",
-      "UCA" => "Serine",
-      "UCG" => "Serine",
-      "UGG" => "Tryptophan",
-      "UAU" => "Tyrosine",
-      "UAC" => "Tyrosine",
-      "UAA" => "STOP",
-      "UAG" => "STOP",
-      "UGA" => "STOP",
-    }
-
-    dict[codon] && {:ok, dict[codon]} || {:error, "invalid codon"}
+    @mappings[codon] && {:ok, @mappings[codon]} || {:error, "invalid codon"}
   end
 end
