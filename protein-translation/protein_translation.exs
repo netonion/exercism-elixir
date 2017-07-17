d47fd81d9c5930401871aa4f399a41f8f2284e5a
Sam Guo <netonion@hotmail.com>
2017-07-16 20:46:41 -0400

    Exercise 6

---
+++ protein-translation/protein_translation.exs
@@ -0,0 +1,69 @@
+defmodule ProteinTranslation do
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: { atom,  list(String.t()) }
+  def of_rna(rna) do
+    proteins = rna
+    |> String.to_charlist
+    |> Enum.chunk(3)
+    |> Enum.map(&List.to_string/1)
+    |> Enum.map(&of_codon/1)
+
+    if Enum.any?(proteins, fn {x, _} -> x == :error end) do
+      {:error, "invalid RNA"}
+    else
+      {:ok,
+        proteins
+        |> Enum.map(fn {_, x} -> x end)
+        |> Enum.take_while(&(&1 != "STOP"))
+      }
+    end
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: { atom, String.t() }
+  def of_codon(codon) do
+    dict = %{
+      "UGU" => "Cysteine",
+      "UGC" => "Cysteine",
+      "UUA" => "Leucine",
+      "UUG" => "Leucine",
+      "AUG" => "Methionine",
+      "UUU" => "Phenylalanine",
+      "UUC" => "Phenylalanine",
+      "UCU" => "Serine",
+      "UCC" => "Serine",
+      "UCA" => "Serine",
+      "UCG" => "Serine",
+      "UGG" => "Tryptophan",
+      "UAU" => "Tyrosine",
+      "UAC" => "Tyrosine",
+      "UAA" => "STOP",
+      "UAG" => "STOP",
+      "UGA" => "STOP",
+    }
+
+    dict[codon] && {:ok, dict[codon]} || {:error, "invalid codon"}
+  end
+end
