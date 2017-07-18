feec705cc4092290371239666f9f51f7d61e55a4
Sam Guo <netonion@hotmail.com>
2017-07-18 15:34:24 -0400

    Exercise 16 encoder

---
+++ run-length-encoding/rle.exs
@@ -0,0 +1,37 @@
+defmodule RunLengthEncoder do
+  @doc """
+  Generates a string where consecutive elements are represented as a data value and count.
+  "AABBBCCCC" => "2A3B4C"
+  For this example, assume all input are strings, that are all uppercase letters.
+  It should also be able to reconstruct the data into its original form.
+  "2A3B4C" => "AABBBCCCC"
+  """
+  @spec encode(String.t) :: String.t
+  def encode(string) do
+    # reduce string to [{A, 2}, {B, 3}, ...] in reversed order
+    reducer = fn x, acc ->
+      case acc do
+        [] -> [{x, 1}]
+        [{^x, n} | rest] -> [{x, n + 1} | rest]
+        _ -> [{x, 1} | acc]
+      end
+    end
+
+    mapper = fn
+      {x, 1} -> x
+      {x, n} -> "#{n}#{x}"
+    end
+
+    string
+    |> String.codepoints
+    |> Enum.reduce([], reducer)
+    |> Enum.reverse
+    |> Enum.map(mapper)
+    |> Enum.join
+  end
+
+  @spec decode(String.t) :: String.t
+  def decode(string) do
+
+  end
+end
