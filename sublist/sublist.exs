a96175d9c86b3c8c6441d61ace2a9814f55d9f37
Sam Guo <netonion@hotmail.com>
2017-07-19 14:27:05 -0400

    Exercise 17 with recursion

--- sublist/sublist.exs
+++ sublist/sublist.exs
@@ -4,16 +4,16 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
-    d = length(a) - length(b)
     cond do
       a == b -> :equal
-      d < 0 && Enum.any?(0..-d, &check(&1, a, b)) -> :sublist
-      d > 0 && Enum.any?(0..d, &check(&1, b, a)) -> :superlist
+      length(a) < length(b) && check(a, b) -> :sublist
+      length(a) > length(b) && check(b, a) -> :superlist
       true -> :unequal
     end
   end
 
-  def check(pos, short, long) do
-    Enum.slice(long, pos, length(short)) === short
+  def check(s, l) when length(s) > length(l), do: false
+  def check(s, l) do
+    Enum.take(l, length(s)) === s || check(s, tl(l))
   end
 end
