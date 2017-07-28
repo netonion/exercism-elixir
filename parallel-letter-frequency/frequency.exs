72dafdde42bb175e24e83a918e369f77399d64b1
Sam Guo <netonion@hotmail.com>
2017-07-28 00:21:33 -0400

    Exercise 44 with concurrent processes

--- parallel-letter-frequency/frequency.exs
+++ parallel-letter-frequency/frequency.exs
@@ -9,10 +9,15 @@ defmodule Frequency do
   @spec frequency([String.t], pos_integer) :: map
   def frequency(texts, workers) do
     texts
-    |> Enum.join
-    |> String.downcase
-    |> String.replace(~r/[\W\d]/u, "")
-    |> String.graphemes
-    |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1+1)) end)
+    |> Task.async_stream(fn text ->
+      text
+      |> String.downcase
+      |> String.replace(~r/[\W\d]/u, "")
+      |> String.graphemes
+      |> Enum.reduce(%{}, fn char, acc -> Map.update(acc, char, 1, &(&1 + 1)) end)
+    end, max_concurrency: workers)
+    |> Enum.reduce(%{}, fn {:ok, res}, acc ->
+      Map.merge(acc, res, fn _k, v1, v2 -> v1 + v2 end)
+    end)
   end
 end
