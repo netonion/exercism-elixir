f6958ea3f0736ce5a272de9a48433c377e1c5f83
Sam Guo <netonion@hotmail.com>
2017-07-28 12:05:17 -0400

    Exercise 45 with recursion

--- binary/binary.exs
+++ binary/binary.exs
@@ -6,10 +6,10 @@ defmodule Binary do
   """
   @spec to_decimal(String.t) :: non_neg_integer
   def to_decimal(string) do
-    try do
-      String.to_integer(string, 2)
-    rescue
-      ArgumentError -> 0
-    end
+    decimal(string, 0)
   end
+  defp decimal("", acc), do: acc
+  defp decimal("1" <> rest, acc), do: decimal(rest, 2 * acc + 1)
+  defp decimal("0" <> rest, acc), do: decimal(rest, 2 * acc)
+  defp decimal(_, _), do: 0
 end
