5cc0c37c060ab2046abff1808082802ecfdd1934
Sam Guo <netonion@hotmail.com>
2017-08-06 19:34:40 -0400

    All your base with tail recursion

--- all-your-base/all-your-base.exs
+++ all-your-base/all-your-base.exs
@@ -6,13 +6,13 @@ defmodule AllYourBase do
 
   @spec convert(list, integer, integer) :: list
   def convert(digits, base_a, base_b) when digits == [] or base_a < 2 or base_b < 2, do: nil
-  def convert(digits, base_a, base_b), do: digits |> to_i(base_a, 0) |> to_b(base_b)
+  def convert(digits, base_a, base_b), do: digits |> to_i(base_a, 0) |> to_b(base_b, [])
 
   defp to_i([], _, acc), do: acc
   defp to_i([head | tail], base, acc) when head in 0..base-1, do: to_i(tail, base, acc * base + head)
   defp to_i(_, _, _), do: nil
 
-  defp to_b(nil, _), do: nil
-  defp to_b(n, base) when n < base, do: [n]
-  defp to_b(n, base), do: to_b(div(n, base), base) ++ [rem(n, base)]
+  defp to_b(nil, _, _), do: nil
+  defp to_b(n, base, acc) when n < base, do: [n | acc]
+  defp to_b(n, base, acc ), do: to_b(div(n, base), base, [rem(n, base) | acc])
 end
